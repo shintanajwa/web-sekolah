abb33284f1d01c91c4bab7cdfbbb5bf805cd089b
shintanajwa <shintaainun37@gmail.com>
2025-02-05 12:03:13 +0700

    Initial Commit

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "sekolah";
+
+// Membuat Koneksi ke database
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+// Cek koneksi
+if(!$koneksi){
+    die("Koneksi gagal; ". mysqli_connect_error());
+}
+
+?>
